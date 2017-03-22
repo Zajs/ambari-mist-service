d456e8ae0fb7a26a1461d93ba6f59ed4b49779a4
Igor Churmeev <ingvarch@gmail.com>
2017-03-22 15:38:32 +0300

    Fixes in python scripts and metainfo

--- package/scripts/master.py
+++ package/scripts/master.py
@@ -3,3 +3,16 @@
 import sys, os, pwd, signal, time
 from resource_management import *
 from subprocess import call
+
+
+class Master(Script):
+  def install(self, env):
+    # Install packages listed in metainfo.xml
+    self.install_packages(env)
+    self.configure(env)
+
+    cmd = 'ps aux'
+    Execute(cmd)
+
+if __name__ == "__main__":
+  Master().execute()
