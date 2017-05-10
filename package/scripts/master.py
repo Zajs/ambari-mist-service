92f2131d91effbeb3e4ce46cef889d96c69e1b1f
Igor Churmeev <ingvarch@gmail.com>
2017-05-10 15:01:05 +0300

    Added configs

--- package/scripts/master.py
+++ package/scripts/master.py
@@ -8,9 +8,21 @@ from subprocess import call
 class Master(Script):
 
   def install(self, env):
-    print 'Install the Sample Srv Master';
+    import params
+    env.set_params(params)
+
+    print 'Install the Mist Master';
     # Install packages listed in metainfo.xml
     self.install_packages(env)
+
+    Execute('find ' + params.service_packagedir + ' -iname "*.sh" | xargs chmod +x')
+
+   # create the log, pid, mits dirs
+    Directory([params.mist_pid_dir, params.mist_log_dir, params.mist_dir],
+              owner=params.mist_user,
+              group=params.mist_group,
+              recursive=True
+              )
 
     print 'Getting mist'
     urllib.urlretrieve ("http://35.157.13.60/mist.tar.gz", "/tmp/mist.tar.gz")
@@ -24,13 +36,30 @@ class Master(Script):
     Execute ('cp /var/lib/ambari-server/resources/stacks/HDP/2.5/services/MIST/package/scripts/mist_service.sh /etc/init.d/mist')
     Execute ('chmod +x /etc/init.d/mist')
 
+    Execute(format("{service_packagedir}/scripts/setup_snapshot.sh {mist_dir} "
+                   "{mist_host} {mist_port} {setup_view} {service_packagedir} "
+                   "{java64_home} >> {mist_log_file}"),
+            user=params.mist_user)
+
+    # if zeppelin installed on ambari server, copy view jar into ambari views dir
+    if params.setup_view:
+        if params.ambari_host == params.mist_internalhost and not os.path.exists(
+                '/var/lib/ambari-server/resources/views/mist-view-1.0-SNAPSHOT.jar'):
+            Execute('echo "Copying mist view jar to ambari views dir"')
+
+
+            Execute(format("cp /var/lib/ambari-server/resources/stacks/HDP/2.5/services/MIST/package/scripts/*.jar "
+                           "/var/lib/ambari-server/resources/views"), ignore_failures=True)
+            #Execute(format("cp /home/zeppelin/*.jar "
+            #               "/var/lib/ambari-server/resources/views"), ignore_failures=True)
+
 
   def stop(self, env):
-    print 'Stop the Sample Srv Master';
+    print 'Stop the Mist Master';
     Execute ('service mist stop')
 
   def start(self, env):
-    print 'Start the Sample Srv Master';
+    print 'Start the Mist Master';
     Execute ('service mist start')
 
   def status(self, env):
@@ -41,7 +70,7 @@ class Master(Script):
     check_process_status(pid_file)
 
   def configure(self, env):
-    print 'Configure the Sample Srv Master';
+    print 'Configure the Mist Master';
 
 if __name__ == "__main__":
   Master().execute()
--- package/scripts/params.py
+++ package/scripts/params.py
@@ -1,3 +1,31 @@
 #!/usr/bin/env python
 from resource_management import *
 import os
+
+# server configurations
+config = Script.get_config()
+
+# e.g. /var/lib/ambari-agent/cache/stacks/HDP/2.5/services/MIST/package
+service_packagedir = os.path.realpath(__file__).split('/scripts')[0]
+
+
+mist_dirname = 'mist'
+install_dir = '/usr/share'
+
+mist_host = config['configurations']['mist-ambari-config']['mist.host.publicname']
+
+# params from mist-env
+mist_user = config['configurations']['mist-env']['mist_user']
+mist_group = config['configurations']['mist-env']['mist_group']
+mist_log_dir = config['configurations']['mist-env']['mist_log_dir']
+mist_pid_dir = config['configurations']['mist-env']['mist_pid_dir']
+mist_log_file = os.path.join(mist_log_dir, 'mist-setup.log')
+
+mist_dir = os.path.join(*[install_dir, mist_dirname])
+conf_dir = os.path.join(*[install_dir, mist_dirname, 'conf'])
+
+# detect configs
+master_configs = config['clusterHostInfo']
+java64_home = config['hostLevelParams']['java_home']
+ambari_host = str(master_configs['ambari_server_host'][0])
+mist_internalhost = str(master_configs['mist_master_hosts'][0])
